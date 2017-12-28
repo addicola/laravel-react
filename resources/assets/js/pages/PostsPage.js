1d4cbdd6e4ca0a50476cc49be49fd63cd50a71e1
pipnyst <blue_lifesky@hotmail.com>
2017-12-28 14:45:44 +0700

    add git ignore

---
+++ resources/assets/js/pages/PostsPage.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Post from "../components/Post";
+
+class PostsPage extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <div className="col-md-8">
+                    fdsfds
+                </div>
+                <div className="col-md-4">
+                    Suggestion
+                </div>
+            </div>
+
+
+        );
+    }
+}
+
+export default PostsPage;
